36e06a6389817c81ce36fa4878a20ef9b9e6122b
byhag <byhag4007@gmail.com>
2016-05-11 02:15:54 -0500

    Further setup: temp files to fill folders

---
+++ Dandy/Assets/Scripts/MoveImage.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveImage : MonoBehaviour {
+
+	private bool goLeft = true;
+	public Vector3 leftBound;
+	public Vector3 rightBound;
+	public float speed = 1f;
+	private float startTime;
+	private float distance;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		distance = Vector3.Distance(leftBound, rightBound);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float distCovered = (Time.time - startTime) * speed;
+		float fracOfDist = distCovered / distance;
+
+		if (goLeft)
+			transform.position = Vector3.Lerp(rightBound, leftBound, fracOfDist);
+		else
+			transform.position = Vector3.Lerp(leftBound, rightBound, fracOfDist);
+
+		if (transform.position.x <= leftBound.x) {
+			goLeft = false;
+			startTime = Time.time;
+		}
+		else if (transform.position.x >= rightBound.x)  {
+			goLeft = true;
+			startTime = Time.time;
+		}
+			
+	}
+}
